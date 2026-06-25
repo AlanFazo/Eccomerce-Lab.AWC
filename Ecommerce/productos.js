faed4cf02ee376612c1e26ee6ccbad054940a502
AlanFazo <alan.rusch@istea.com.ar>
2026-06-25 20:22:11 -0300

    Mejoras visuales y agrego de modal

--- Ecommerce/productos.js
+++ Ecommerce/productos.js
@@ -7,18 +7,56 @@ fetch('https://fakestoreapi.com/products')
 
 function renderCards(productos) {
     productos.forEach(producto => {
+         const col = document.createElement("div");
+        col.classList.add("col");
         const card = document.createElement("div");
         card.classList.add("card");
         
         card.innerHTML = `
-        <img src="${producto.image}" alt="${producto.title}" class="card-image">
-        <div class="card-content">
-            <h3 class="card-title">${producto.title}</h3>
-            <p class="card-description">${producto.description}</p>
-            <p class="card-price">${producto.price}</p>
+        <img src="${producto.image}" alt="${producto.title}" class="card-image" style="height: 300px;object-fit: contain"> 
+        <div class="card-content justify-content-center align-items-center" style="width: 300px; padding: 10px;">
+            <h3 class="card-title text-truncate">${producto.title}</h3>
+            <div class="mb-3">
+            <button class="btn btn-dark" id="btn-${producto.id}"> Mas detalles </button>
+            </div>
       </div>
     `;
 
-contenedorProductos.appendChild(card);
+
+
+  
+  
+
+  col.appendChild(card);
+  contenedorProductos.appendChild(col);
+
+
+            let container = document.querySelector('#productmodal')
+          let btn = document.querySelector(`#btn-${producto.id}`);
+      btn.addEventListener('click', () => {
+        container.innerHTML =  `
+          <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="productmodalLabel">${producto.title}</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       <img src="${producto.image}" class="img-fluid" alt="${producto.title}">
+        <p>${producto.description}"</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+      </div>`;
+      
+      const boostrapmodal = new bootstrap.Modal(container);
+      boostrapmodal.show();
+    });
+
+
+
 });
 }
